a780e7639a7d7ecbb60f3cf1b2dcddf6ef1cd11e
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-02-17 11:19:20 -0300

    feat: created users/avatar route to upload files to /src/tmp

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Request, Response, Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import CreateUserService from '@src/services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req: Request, res: Response) => {
   try {
@@ -14,5 +19,14 @@ usersRouter.post('/', async (req: Request, res: Response) => {
     return res.status(400).json({ message: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req: Request, res: Response) => {
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
