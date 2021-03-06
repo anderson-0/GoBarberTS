962f05692089463443a795eb66958fb4cc503246
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-03-06 17:55:27 -0300

    fix: forgot to include password routes into main routes file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response, Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 routes.get('/', (req: Request, res: Response, _next: NextFunction) => {
   const { email, password } = req.body;
