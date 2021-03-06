3b317842b4723815419132b6ca783dc58f74b50b
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-03-06 17:52:33 -0300

    feat: implemented findByToken function inside FakeUserTokenRepository and added created_at property to userToken object inside the generate function

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -7,9 +7,19 @@ class FakeUserTokensRepository implements IUserTokensRepository {
 
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
-    Object.assign(userToken, { id: uuid(), token: uuid(), user_id });
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+      created_at: Date.now(),
+    });
 
     this.userTokens.push(userToken);
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(t => t.token === token);
     return userToken;
   }
 }
