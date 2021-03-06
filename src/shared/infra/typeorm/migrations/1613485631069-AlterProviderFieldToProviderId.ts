752a0a41b44aca3a8dc75a11d499b89d2acbea7a
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-03-06 18:12:19 -0300

    fix: added default export to migration

--- src/shared/infra/typeorm/migrations/1613485631069-AlterProviderFieldToProviderId.ts
+++ src/shared/infra/typeorm/migrations/1613485631069-AlterProviderFieldToProviderId.ts
@@ -5,7 +5,7 @@ import {
   TableForeignKey,
 } from 'typeorm';
 
-export class AlterProviderFieldToProviderId1613485631069
+export default class AlterProviderFieldToProviderId1613485631069
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('appointments', 'provider');
