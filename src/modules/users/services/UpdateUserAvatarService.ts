b676177ad924f57f30314a17108e0165f653ef71
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-03-07 17:16:02 -0300

    refactor: removed unused imports from UpdateUserAvatarService

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import { inject, injectable } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
