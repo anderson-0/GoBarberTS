58673ff57ed33de15bcdca4175e570f746093138
Anderson Mattjie da Silva <anderson.consultant.it@outlook.com>
2021-04-07 16:58:35 -0300

    chore: included SES mail provider and added environment variables for aws

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,13 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
 import FakeMailProvider from './MailProvider/fakes/FakeMailProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 container.registerSingleton('StorageProvider', DiskStorageProvider);
 container.registerSingleton('MailProvider', FakeMailProvider);
@@ -15,5 +18,7 @@ container.registerSingleton<IMailTemplateProvider>(
 );
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
